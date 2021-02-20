48cca1b90eb3eec943d6a5f6f633209a0da0433e
Timothée Sterle <timothee-s@mailoo.org>
2021-02-20 19:06:05 +0100

    refactor(user_options): use or instead of if check

--- lua/sqls/user_options.lua
+++ lua/sqls/user_options.lua
@@ -1,13 +1,9 @@
 local M = {}
 
-
 function M.init(opts)
     function M.picker(...)
         local pickers = require('sqls.pickers')
-        if pickers[opts.picker] then
-            return pickers[opts.picker](...)
-        end
-        return pickers.default(...)
+        return (pickers[opts.picker] or pickers.default)(...)
     end
 end
 
